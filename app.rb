f0a8467c98aac5ecabe4d741abd4ca8c2c118924
Martin Jaime Flores Jr <mf34314@gmail.com>
2019-02-18 12:26:38 -0600

    Added Base Routes
    More routes may be added or current ones modified later

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
-# ///////////////////////////
+# ---------------------------
 # Requires | Gems, DBs, Funcs
-# ///////////////////////////
+# ---------------------------
 require 'sinatra'
 require 'data_mapper'
 
-# ////////////////////////
+# ------------------------
 # IGNORE | Database Config
-# ////////////////////////
+# ------------------------
 if ENV['DATABASE_URL']
   DataMapper::setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 else
@@ -15,9 +15,29 @@ end
 
 DataMapper.finalize
 
-# //////////////////////////
-# Request Handlers | General
-# //////////////////////////
-get "/" do
-  
+# ----------------
+# Request Handlers
+# ----------------
+post "/register_user" do
+  "NO"
+end
+
+post "/authenticate_user" do
+  "NO"
+end
+
+get "/get_helpers" do
+  "NO"
+end
+
+get "/get_VIP" do
+  "NO"
+end
+
+get "/request_helper" do
+  "NO"
+end
+
+get "/request_VIP" do
+  "NO"
 end
