a32cfcfddbcdb5b61f082ffc5270ad96c4f9aa01
Martin Jaime Flores Jr <mf34314@gmail.com>
2019-03-10 11:42:49 -0500

    Modify tokens, update Expired logic

--- tokens.rb
+++ tokens.rb
@@ -2,14 +2,16 @@ require 'data_mapper'
 
 class Tokens
     include DataMapper::Resource
-    property :user_id, Integer, :key => true
+    property :user_id, Integer
     property :created_at, DateTime
     property :expires, DateTime
     property :user_key, String
-    property :UUID, String
+    property :UUID, String, :key => true
     
     def isExpired()
         now = Time.now
-        return now > self.expires
+        print now
+        print self.expires.to_time
+        return now > self.expires.to_time
     end
 end
