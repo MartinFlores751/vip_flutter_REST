c620222b485bcd3a2c62529980c3c9ecbad09d4b
Martin Jaime Flores Jr <mf34314@gmail.com>
2019-03-11 13:15:13 -0500

    Add Online Check

--- tokens.rb
+++ tokens.rb
@@ -7,9 +7,16 @@ class Tokens
     property :expires, DateTime
     property :user_key, String
     property :UUID, String, :key => true
+    property :last_request, DateTime
     
     def isExpired()
         now = Time.now
         return now > self.expires.to_time
+    end
+
+    def isOnline()
+        now = Time.now
+        offline_time = self.last_request + 300
+        return now > offline_time
     end
 end
