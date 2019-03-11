3d382822617ec6e45d9db95964902cadd50e8014
Martin Jaime Flores Jr <mf34314@gmail.com>
2019-03-11 13:11:21 -0500

    Return Helpers

--- app.rb
+++ app.rb
@@ -93,7 +93,12 @@ post "/api/authenticate_user" do
 end
 
 get "/api/get_helpers" do
-  "NO"
+  u_js = []
+  users = User.all(:helper => true)
+  users.each do |u|
+    u_js.push(u.user_name)
+  end
+  return u_js.to_json
 end
 
 get "/api/get_VIP" do
