97a4f93d34f39be4b616d97d35528b573ceee21f
Martin Jaime Flores Jr <mf34314@gmail.com>
2019-03-22 13:44:56 -0500

    Fix token thang

--- tokens.rb
+++ tokens.rb
@@ -2,11 +2,11 @@ require 'data_mapper'
 
 class Tokens
     include DataMapper::Resource
-    property :user_id, Integer
+    property :user_id, Integer, :key => true
     property :created_at, DateTime
     property :expires, DateTime
     property :user_key, String
-    property :UUID, String, :key => true
+    property :UUID, String
     
     def isExpired()
         now = DateTime.now
